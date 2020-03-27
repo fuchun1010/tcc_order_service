d787979571abdd7a00ca9d2f912c1646a2c06bde
fuchun198435163.com <tank198435@163.com>
2020-03-27 14:54:32 +0800

    [fuchun] #bug fix config url bug

--- src/main/java/com/tank/controller/OrderController.java
+++ src/main/java/com/tank/controller/OrderController.java
@@ -4,11 +4,10 @@ import com.tank.common.ApiResult;
 import com.tank.protocol.CoordinatorReq;
 import com.tank.protocol.PreSubMoneyReq;
 import com.tank.protocol.RollbackAccountReq;
-import com.tank.protocol.SubAccountReq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -17,30 +16,31 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/v1")
 public class OrderController {
 
-  @PostMapping("/preSub")
+  @GetMapping("/preSub")
   public ResponseEntity<ApiResult<String>> preSub() {
     ApiResult<String> apiResult = new ApiResult<>();
     //TODO send reserve operation to account_service
 
     String accountReserveUrl = this.environment.getProperty("account_service.reserve.url");
-    String accountCommitUrl = this.environment.getProperty("account_service.reserve.commit");
-    String accountRollbackUrl = this.environment.getProperty("account_service.reserve.rollback");
+    String accountCommitUrl = this.environment.getProperty("account_service.commit.url");
+    String accountRollbackUrl = this.environment.getProperty("account_service.rollback.url");
 
     String coordinatorUrl = this.environment.getProperty("transaction_coordinator.server.url");
 
     PreSubMoneyReq preSubMoneyReq = new PreSubMoneyReq();
     preSubMoneyReq.setAccountId(1).setApp("order_service").setMoney(100).setXid("s0001");
 
-    ResponseEntity<ApiResult> result = this.restTemplate.postForEntity(accountReserveUrl, preSubMoneyReq, ApiResult.class);
-    String error = result.getBody().getError();
-    String preStatus = error == null ? "preFailure" : "success";
+//    ResponseEntity<ApiResult> result = this.restTemplate.postForEntity(accountReserveUrl, preSubMoneyReq, ApiResult.class);
+//    String error = result.getBody().getError();
+//    String preStatus = error == null ? "preFailure" : "success";
+    String preStatus = "preFailure";
 
     CoordinatorReq coordinatorReq = new CoordinatorReq();
     coordinatorReq.setStatus(preStatus);
 
-    SubAccountReq subAccountReq = new SubAccountReq();
-    subAccountReq.setXid(preSubMoneyReq.getXid());
-    coordinatorReq.addCommit(accountCommitUrl, subAccountReq);
+//    SubAccountReq subAccountReq = new SubAccountReq();
+//    subAccountReq.setXid(preSubMoneyReq.getXid());
+//    coordinatorReq.addCommit(accountCommitUrl, subAccountReq);
 
     RollbackAccountReq rollbackAccountReq = new RollbackAccountReq();
     rollbackAccountReq.setXid(preSubMoneyReq.getXid());
