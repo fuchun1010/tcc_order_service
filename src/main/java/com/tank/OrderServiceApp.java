c8687cd99450986aefff43a2de25f9b34a8fccb6
fuchun198435163.com <tank198435@163.com>
2020-03-27 09:59:29 +0800

    [fuchun] #feat run success

--- src/main/java/com/tank/OrderServiceApp.java
+++ src/main/java/com/tank/OrderServiceApp.java
@@ -1,10 +1,14 @@
 package com.tank;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author tank198435163.com
  */
+@SpringBootApplication
 public class OrderServiceApp {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    SpringApplication.run(OrderServiceApp.class);
   }
 }
